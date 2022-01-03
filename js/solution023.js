715051dcbeef662697f5a89d988128c8c8923980
spideydan <dan.inorio4@gmail.com>
2022-01-03 11:51:38 -0500

    solution023 done

---
+++ js/solution023.js
@@ -0,0 +1,12 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in the same order.
+
+// "h,o,l,a"
+
+
+function printArray(array) {
+    return array.join()
+}
