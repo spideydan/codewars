3d4eac325cf4665114aec70233a09bca27f9faed
spideydan <dan.inorio4@gmail.com>
2022-01-16 13:02:02 -0500

    solution036 done

---
+++ js/solution036.js
@@ -0,0 +1,23 @@
+// Write a function that takes a single string(word) as argument.The function must return an ordered list containing the indexes of all capital letters in the string.
+
+//     Example
+// Test.assertSimilar(capitals('CodEWaRs'), [0, 3, 4, 6]);
+
+var capitals = function (word) {
+    //P strings
+    //R returning numbers in array
+    //E ('CodEWaRs'), [0,3,4,6]
+    //P 
+    /* take in string
+    loop through string
+    find where the index of the string is
+    return number of indexes in array */
+
+    let emptyArr = []
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === word[i].toUpperCase()) {
+            emptyArr.push(i)
+        }
+    }
+    return emptyArr
+}
